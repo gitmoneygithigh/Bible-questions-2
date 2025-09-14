ecaed556b103bddfabdec632e034807c400a78bd
gitmoneygithigh <172468488+gitmoneygithigh@users.noreply.github.com>
2025-09-14 06:48:44 -0500

    edge bug fix

--- newScript.js
+++ newScript.js
@@ -4,90 +4,99 @@ document.addEventListener("DOMContentLoaded", function() {
     const movableDivWidth = document.getElementById('movable-div').offsetWidth;
     movableDiv.style.backgroundColor = 'grey';
     var globalID;
-    var stepSize = 2;
-    var shift = 0;
+    var stepSize = 5;
     var maxPosition = gameDivWidth - movableDivWidth;
     var rightKeyPressed = false;
     var leftKeyPressed = false;
-    var firstKeyPressed = "No info";
-    var moving = false;
+    var lastKeyPressed = "No info";
+    STATIONARY = "STATIONARY";
+    var condition = STATIONARY;
+    let shift = 350;
+    movableDiv.style.transform = `translateX(${shift}px)`;
+    ArrowRight = "ArrowRight";
+    MOVING_RIGHT = "MOVING_RIGHT";
+    ArrowLeft = "ArrowLeft";
+    MOVING_LEFT = "MOVING_LEFT";
+    function checkEdgeCase() {
+        if (shift >= maxPosition) {
+            shift = maxPosition;
+        } else if (shift <= 0) {
+            shift = 0;
+        }
+    }
+    function checkCondition() {
+        if (!rightKeyPressed && !leftKeyPressed) {
+            condition = STATIONARY;
+        }
+        if (rightKeyPressed && leftKeyPressed) {
+            if (lastKeyPressed === ArrowRight) {
+                condition = MOVING_RIGHT;
+            } else if (lastKeyPressed === ArrowLeft) {
+                condition = MOVING_LEFT;
+            }
+        }
+        if (rightKeyPressed && !leftKeyPressed) {
+               condition = MOVING_RIGHT;
+        }
+        if (leftKeyPressed && !rightKeyPressed) {
+            condition = MOVING_LEFT;
+        }
+    }
+    function calculateShift () {
+        if (condition === MOVING_RIGHT) {
+            shift += stepSize;
+        } else if (condition === MOVING_LEFT) {
+            shift -= stepSize;
+        } 
+        if (shift > maxPosition) {
+            shift = maxPosition;
+        } else if (shift < 0) {
+            shift = 0;
+        }
+    }
+    function makeMove() {
+        movableDiv.style.transform = `translateX(${shift}px)`;
+        if (condition === MOVING_RIGHT || condition === MOVING_LEFT) {
+            globalID = requestAnimationFrame(step);
+        }
+    }
     function showInfo() {
         console.log(globalID + " is globalID");
         console.log(shift + " is shift");
         console.log(rightKeyPressed + " is rightKeyPressed");
         console.log(leftKeyPressed + " is leftKeyPressed");
-        console.log(firstKeyPressed + " is firstKeyPressed");
-        console.log(moving + " is moving");
+        console.log(condition + " is condition");
+        console.log(lastKeyPressed + " is last pressed key");
         console.log(" ");
     }
-    shift = 350;
-    movableDiv.style.transform = `translateX(${shift}px)`;
     function step() {
-        if (rightKeyPressed && leftKeyPressed) {
-            if (firstKeyPressed === "ArrowRight") {
-                shift += stepSize;
-            } else if (firstKeyPressed === "ArrowLeft") {
-                shift -= stepSize;
-            }   
-        } else if (firstKeyPressed === "ArrowRight") {
-            shift += stepSize;
-        } else if (firstKeyPressed === "ArrowLeft") {
-            shift -= stepSize;
-        }
-        if (rightKeyPressed) {
-            if (shift > maxPosition - stepSize) {
-                shift = maxPosition;
-                cancelAnimationFrame(globalID);
-                moving = false;
-                console.log('right edge');
-                return;
-            }
-        } else if (leftKeyPressed) {
-            if (shift < stepSize) {
-                shift = 0;
-                cancelAnimationFrame(globalID);
-                moving = false;
-                console.log('left edge');
-                return;
-            }
-        } else if (!rightKeyPressed && !leftKeyPressed) {
-            cancelAnimationFrame(globalID);
-            moving = false;
-            console.log('stopped because no keys pressed');
-            showInfo();
-            return;
-        }
-        console.log(firstKeyPressed + " is first pressed key")
-        movableDiv.style.transform = `translateX(${shift}px)`;
-        globalID = requestAnimationFrame(step);
+        checkEdgeCase();
+        checkCondition();
+        calculateShift();
+        makeMove ();
         showInfo();
     }
     document.addEventListener('keyup', function(event) {
-        if (event.key === "ArrowRight" || event.key === "ArrowLeft") {
-            if (event.key === "ArrowRight") {
-                if (leftKeyPressed && firstKeyPressed === "ArrowRight") {
-                    firstKeyPressed = "ArrowLeft";
-                }
+        if (event.key === ArrowRight || event.key === ArrowLeft) {
+            if (event.key === ArrowRight) {
                 rightKeyPressed = false;
-            } else if (event.key === "ArrowLeft") {
-                if (rightKeyPressed && firstKeyPressed === "ArrowLeft") {
-                    firstKeyPressed = "ArrowRight";
-                }
+            } else if (event.key === ArrowLeft) {
                 leftKeyPressed = false;
             }
         }
     });
     document.addEventListener('keydown', function(event) {
-        if (event.key === "ArrowRight") {
-            firstKeyPressed = "ArrowRight";
-            rightKeyPressed = true;
-        } else if (event.key === "ArrowLeft") {
-            firstKeyPressed = "ArrowLeft";
-            leftKeyPressed = true;
-        }
-        if (!moving) {
-            globalID = requestAnimationFrame(step);
-            moving = true;
+        if (event.key === ArrowRight || event.key === ArrowLeft) {
+            if (event.key === ArrowRight) {
+                    lastKeyPressed = ArrowRight;
+                    rightKeyPressed = true;
+            } else if (event.key === ArrowLeft) {
+                    lastKeyPressed = ArrowLeft;
+                    leftKeyPressed = true;
+            }
+            if (condition === STATIONARY) {
+                globalID = requestAnimationFrame(step);
+            }
         }
     });
 });
